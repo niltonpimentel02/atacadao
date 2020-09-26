9f4f7e2a47b3d1472000c4945f09aad32767383b
Nilton Pimentel <niltonpimentel02@gmail.com>
2020-09-26 16:02:51 -0300

    Criada landing page
    Close #21

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -1,6 +1,18 @@
-from django.test import Client
+# from django.test import Client
+#
+#
+# def test_status_code(client: Client):
+#     resp = client.get('/')
+#     assert resp.status_code == 200
+import pytest
+from django.urls import reverse
 
 
-def test_status_code(client: Client):
-    resp = client.get('/')
+@pytest.fixture
+def resp(client):
+    resp = client.get(reverse('home'))
+    return resp
+
+
+def test_status_code(resp):
     assert resp.status_code == 200
